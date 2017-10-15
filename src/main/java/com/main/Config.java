84755609ea1f974997a764aa10ac85736269ea6c
齐放 <qi.fang@neusoft.com>
2017-10-15 17:29:21 +0800

    多线程

--- src/main/java/com/main/Config.java
+++ src/main/java/com/main/Config.java
@@ -1,11 +1,36 @@
 package com.main;
 
+import java.util.concurrent.Executor;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 
 @Configuration
-@ComponentScan("com.main")
-public class Config {
+@ComponentScan("com.thread")
+@EnableAsync//开启异步任务支持
+public class Config implements AsyncConfigurer {
+
+	//配置类实现AsyncConfigurer接口并重写getAsyncExecutor方法，并返回一个ThreadPoolTaskExecutor
+	//这样就获得了一个基于线程池TaskExecutor
+	@Override
+	public Executor getAsyncExecutor() {
+		ThreadPoolTaskExecutor  taskExecutor = new ThreadPoolTaskExecutor();
+		taskExecutor.setCorePoolSize(5);
+		taskExecutor.setMaxPoolSize(5);
+		taskExecutor.setQueueCapacity(55);
+		taskExecutor.initialize();
+		return taskExecutor;
+	}
+
+	@Override
+	public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- src/main/java/com/main/Main.java
+++ src/main/java/com/main/Main.java
@@ -2,12 +2,19 @@ package com.main;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.thread.AsyncTaskService;
+
 
 public class Main {
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext context= 
-				new AnnotationConfigApplicationContext();
+				new AnnotationConfigApplicationContext(Config.class);
+		AsyncTaskService asyncTaskService = context.getBean(AsyncTaskService.class);
 		
+		for(int i =0 ; i<10 ;i++) {
+			asyncTaskService.executeAsyncTask(i);
+			asyncTaskService.executeAsyncTaskAdd(i);
+		}
 		
 		context.close();
 	}
