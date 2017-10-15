cef6d44b0b80284e953e5806fc9a33eba1750791
齐放 <qi.fang@neusoft.com>
2017-10-15 17:07:32 +0800

    init

--- src/main/java/com/main/Main.java
+++ src/main/java/com/main/Main.java
@@ -2,22 +2,12 @@ package com.main;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import com.frank.aop.AopConfig;
-import com.frank.aop.annotaton.DemoAnnotationService;
-import com.frank.aop.method.DemoMethodService;
 
 public class Main {
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext context= 
-				new AnnotationConfigApplicationContext(AopConfig.class);
+				new AnnotationConfigApplicationContext();
 		
-		DemoAnnotationService demoAnnotationService = context.getBean(DemoAnnotationService.class);
-		
-		DemoMethodService demoMethodService = context.getBean(DemoMethodService.class);
-		
-		demoAnnotationService.add();
-		
-		demoMethodService.add();
 		
 		context.close();
 	}
